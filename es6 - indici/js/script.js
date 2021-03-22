7693f06d21663d7825dad54cfc53888e5a063dd1
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-22 18:00:59 +0100

    Fix

--- es6 - indici/js/script.js
+++ es6 - indici/js/script.js
@@ -13,9 +13,14 @@ const userArray = [ 'pappa', 'ciccia', 1, 2, 'francesco', 'giagguaro' ]
 
 // const interval = printInterval( userArray, num1, num2);
 
+
+// Si può anche aggiungere index dopo element prima della arrow function per evitare di urtilizzare indexof
 let interval = userArray.filter( element => userArray.indexOf(element) <= max && userArray.indexOf(element) >= min );
 
-
+// Correzione
+// let interval = userArray.filter( (element, index) => {
+//   return userArray.indexOf(element) <= max && userArray.indexOf(element) >= min
+// };
 
 
 console.log( `L' intervallo è composto da: ${interval}` );
